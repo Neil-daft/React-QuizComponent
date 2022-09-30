6d39e5af7660742594e61e989b8c59e875e40af3
Neil McMahon <neil.mcmahon@distilled.ie>
2022-09-30 14:30:55 +0100

    quizend, conditional

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,19 +1,29 @@
 import React, {Component} from "react";
 import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
 let quizData = require('./quiz_data.json');
 
-class Quiz extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {quiz_position: 1};
-    }
 
-    render() {
-        return (
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-        )
+    class Quiz extends Component {
+
+        constructor(props) {
+            super(props);
+            this.state = {quiz_position: 1};
+        }
+
+        render() {
+            const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+            let quiz = (isQuizEnd)
+                ?  <QuizEnd/>
+                : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>;
+
+            return (
+                <div>
+                    {quiz}
+                </div>
+            )
+        }
     }
-}
 
 export default Quiz;
